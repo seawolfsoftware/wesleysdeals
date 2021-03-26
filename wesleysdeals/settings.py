9548811b509be41e31097596839e4533ba3a858a
Chaz Selph <chaz@seawolfsoftware.io>
2021-03-25 20:05:09 -0400

    restyle admin dashboard, login

--- wesleysdeals/settings.py
+++ wesleysdeals/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'wesleysdeals.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'wesleysdeals/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
