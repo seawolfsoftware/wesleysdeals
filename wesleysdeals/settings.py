193790c3f1e3a70305734f690d78899270cf9ffa
seawolfsoftware <chaz@seawolfsoftware.io>
2021-03-29 22:04:14 +0000

    commit from server

--- wesleysdeals/settings.py
+++ wesleysdeals/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'iodlrzlck5wqyj&l6hr1z@$hqbs@9^05qzm70y0s)dt0dnwr*y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['68.183.152.95', 'localhost']
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
