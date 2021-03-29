151c9bd3cf401bb25850255feccedecdb6ae14eb
Chaz Selph <chaz@seawolfsoftware.io>
2021-03-29 18:29:37 -0400

    add footer

--- wesleysdeals/settings.py
+++ wesleysdeals/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'iodlrzlck5wqyj&l6hr1z@$hqbs@9^05qzm70y0s)dt0dnwr*y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['68.183.152.95', 'localhost']
+ALLOWED_HOSTS = ['68.183.152.95', 'localhost', '192.168.0.20']
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
