7c29ae666168396ca621a5760697f5a0ecd080b1
Chaz Selph <chaz@seawolfsoftware.io>
2021-04-05 00:03:02 -0400

    designing widget form

--- wesleysdeals/settings.py
+++ wesleysdeals/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'iodlrzlck5wqyj&l6hr1z@$hqbs@9^05qzm70y0s)dt0dnwr*y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['68.183.152.95', 'localhost']
+ALLOWED_HOSTS = ['68.183.152.95', 'localhost', '127.0.0.1']
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
@@ -138,3 +138,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'wesleysdeals/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'info@seawolfsoftware.io'
+EMAIL_HOST_PASSWORD = '123god1;'
