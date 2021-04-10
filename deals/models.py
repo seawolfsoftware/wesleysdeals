cb609ada966740ecc534d51531c57d6dda422e16
Chaz Selph <chaz@seawolfsoftware.io>
2021-04-10 01:03:09 -0400

    add test products

--- deals/models.py
+++ deals/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User, Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 from datetime import datetime, timedelta
 
@@ -17,22 +18,36 @@ class Deal(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='images/', default='default.jpg')
+    is_multi_use = models.BooleanField(default=False)
+
+    class Meta:
+        permissions = (
+            ('can_join_group', "Can join group"),
+
+        )
+
+    if is_multi_use:
+
+        group_name = "group{}".format(title)
+        new_multi_use_code_group, created = Group.objects.get_or_create(name=group_name)
+
 
     # ForeignKey is a many-to-one relationship, many codes can be posted by one deal
     code = models.ForeignKey(Code, on_delete=models.CASCADE, default=None)
+
 
     def __str__(self):
         return self.title
 
 
-
 # class MultiUseDeal(Deal):
 #
+# #
 #     # access date time needs to be when the user accesses the deal, not when deal is posted
 #
-#     access_datetime = models.DateTimeField(default=datetime.now)
-#     expiry_datetimestamp = datetime.now() + timedelta(minutes=15)
-#     expiry_date = models.DateTimeField(expiry_datetimestamp)
+    # access_datetime = models.DateTimeField(default=datetime.now)
+    # expiry_datetimestamp = datetime.now() + timedelta(minutes=15)
+    # expiry_date = models.DateTimeField(expiry_datetimestamp)
 #
 #     @property
 #     def is_expired(self):
@@ -44,6 +59,3 @@ class Deal(models.Model):
     # initiate user into group where max size is max use times
     # one multi-use-code can be used by one group
 
-    # new_multi_use_code_group, created = Group.objects.get_or_create(name='New Multi-Use Code Group')
-    # permission_to_access = Permission.objects.get(name='Permission to access multi-use code')
-    # new_multi_use_code_group.permissions.add(permission_to_access)
--- deals/views.py
+++ deals/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 
 from .models import Deal
 from .serializers import DealSerializer
+from django.contrib.auth.models import User, Group
 
 
 class DealList(generics.ListCreateAPIView):
@@ -12,3 +13,16 @@ class DealList(generics.ListCreateAPIView):
 class DealDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Deal.objects.all()
     serializer_class = DealSerializer
+
+
+# def add_user_to_group():
+#
+#     group_name = "group{}".format(title)
+#     new_multi_use_code_group, created = Group.objects.get_or_create(name=group_name)
+#
+#
+#     user_group = Group.objects.get(name='Mygroup')
+#
+#     user.groups.add(user_group)
+
+
